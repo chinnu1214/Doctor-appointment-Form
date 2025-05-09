d90f5f0afea766f87850f4244673930bbd4e3184
chinnu1214 <pooja.chandrika2004@gmail.com>
2025-05-09 20:16:30 +0530

    App.js

---
+++ App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import AppointmentFormContainer from './components/AppointmentFormContainer';
+function App() {
+  return (
+    <div className='App'>
+      <AppointmentFormContainer/>
+    </div>
+  );
+}
+
+export default App;
